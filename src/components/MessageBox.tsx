c2cbd9a6ac160ca920b75c5ec6b1922ca4e59866
Chitsanuphong Chaihong <chitsanuphongchaihong@outlook.com>
2023-10-11 15:22:20 +0700

    Make UI all

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -2,8 +2,9 @@ interface IMessageBox {
   state: string;
   name: string;
   payload: string;
+  ownerMsg?: boolean;
 }
-export default function MessageBox({ name, payload, state }: IMessageBox) {
+export default function MessageBox({ name, payload, state, ...props }: IMessageBox) {
   const decodeData = (data: string): string => {
     return data;
   };
@@ -12,15 +13,25 @@ export default function MessageBox({ name, payload, state }: IMessageBox) {
     switch (state) {
       case "Connected":
         return (
-          <div>
-            <p>{name} joined your room</p>
+          <div className="flex-center text-xs">
+            <p><span className="text-red-800" style={{letterSpacing: 0.4}}>{name}</span> เข้าร่วมห้องสนทนา</p>
           </div>
         );
     }
+
     return (
-      <div>
-        <h3>{name}</h3>
-        <p>{decodeData(payload)}</p>
+      <div className={`w-full flex flex-col ${props.ownerMsg ? 'items-end' : ''}`}>
+        <div className={`w-min flex flex-col ${props.ownerMsg ? 'text-right' : ''}`}>
+          <p className={`text-xs mr-2 mb-1`}>
+            { name }
+          </p>
+          <p className={`
+             px-3 lg:px-4 py-2 rounded-lg w-min min-w-40 max-w-[90%]
+             ${props.ownerMsg ? 'bg-indigo-500 text-white' : 'bg-gray-200 text-black'}
+          `}>
+            {decodeData(payload)}
+          </p>
+        </div>
       </div>
     );
   };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,8 @@ import { invoke } from "@tauri-apps/api";
 import Swal from "sweetalert2";
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
+import BannerImg from './../assets/img/banner.svg'
+
 
 export default function Home() {
    const { NameState, HostState } = useContext(AppContext);
@@ -30,8 +32,18 @@ export default function Home() {
   const onClickConnectServer = async () => {
     const name = await enterName();
     const host = await enterHost();
-      NameState.set(name);
-      HostState.set(host);
+    // add verify ws pattern
+    if (!host.startsWith("ws://")) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "URL WS ไม่ถูกต้อง, โปรดลองใหม่อีกครั้ง",
+        confirmButtonText: "เข้าใจแล้ว",
+      });
+      return;
+    }
+    NameState.set(name);
+    HostState.set(host);
     if (name.length > 0 && host.length > 0) {
       history("/chat");
     }
@@ -41,19 +53,20 @@ export default function Home() {
     return new Promise((res, _) => {
       Swal.fire({
         icon: "question",
-        title: "Question",
-        text: "Enter Your Name",
+        title: "",
+        text: "ชื่อของคุณในห้องสนทนา",
         input: "text",
         allowOutsideClick: false,
         inputAttributes: {
           autocapitalize: "off",
         },
         showCancelButton: true,
-        confirmButtonText: "Confirm",
+        confirmButtonText: "ยืนยัน",
+        cancelButtonText: "ยกเลิก",
         preConfirm: (value) => {
           if (!value) {
             Swal.showValidationMessage(
-              '<i class="fa fa-info-circle"></i> Your name is required'
+              '<i class="fa fa-info-circle"></i> โปรดระบุชื่อของคุณ'
             );
           }
         },
@@ -69,8 +82,8 @@ export default function Home() {
     return new Promise((res, _) => {
       Swal.fire({
         icon: "question",
-        title: "Question",
-        text: "Enter Your Host",
+        title: "",
+        text: "ระบุ URL ของห้องสนทนา ( ws://....... )",
         input: "text",
         allowOutsideClick: false,
         inputAttributes: {
@@ -81,7 +94,7 @@ export default function Home() {
         preConfirm: (value) => {
           if (!value) {
             Swal.showValidationMessage(
-              '<i class="fa fa-info-circle"></i> Your Host is required'
+              '<i class="fa fa-info-circle"></i> โปรดระบุ URL ของห้องสนทนา'
             );
           }
         },
@@ -94,10 +107,53 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <button onClick={onClickCreateServer}>Create Room</button>
-      <button onClick={onClickConnectServer}>Connect Room</button>
-      Test Push
-    </div>
+    <>
+
+      <div className={`flex flex-col w-full h-screen flex-center hidden`}>
+
+        <p className="-mt-20 lg:-mt-10 text-xl lg:text-xl 2xl:text-2xl" style={{letterSpacing: '0.06rem'}}>
+          Encumcrypt Chat
+        </p>
+        
+        <img 
+          src={BannerImg} alt="#" 
+          className="mt-10 w-8/12 lg:w-4/12 2xl:w-6/12 max-h-[24rem]" 
+        />
+        
+        <div className="mt-10 w-8/12 lg:w-4/12 flex-center flex-col lg:flex-row gap-y-3 gap-x-6 lg:gap-x-4">
+          
+          <button 
+            onClick={onClickConnectServer}
+            className={`
+              flex-center
+              btn text-[0.9rem] lg:text-[1rem] w-full h-10 lg:h-11
+              bg-indigo-500 hover:bg-indigo-700
+              border-transparent shadow-md lg:order-2
+            `}
+            style={{
+              letterSpacing: 0.6
+            }}
+          >
+            เชื่อมต่อห้องแชท
+          </button>
+
+          <button 
+            onClick={onClickCreateServer}
+            className={`
+              flex-center
+              btn text-[0.88rem] lg:text-[1rem] w-full text-black border-black/60
+              hover:bg-gray-200 bg-white border shadow-md h-9 lg:h-11
+            `}
+            style={{
+              letterSpacing: 0.6
+            }}
+          >
+            สร้างห้องแชท
+          </button>
+
+        </div>
+      </div>
+
+    </>
   );
 }
